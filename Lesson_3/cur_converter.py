37264b6c1a691af7d06c3eebd280cd7878c0f46c
DmitrTRC <9984398@gmail.com>
2021-03-21 19:53:25 +0300

    Currency converter: Refactored.

--- Lesson_3/cur_converter.py
+++ Lesson_3/cur_converter.py
@@ -5,7 +5,7 @@ if input('Do you need currency exchange table ? y/n -> ').lower() == 'y':
     for cur in rates.get_rates('RUB'):
         print(f'{cur}   :   {rates.get_rate(cur, "RUB"):.2f} Rub.')
 
-amount = float(input('Enter the ˚amount -> '))
+amount = float(input('Enter the amount -> '))
 source_currency = input('Enter the source currency -> ').upper()
 target_currency = input('Enter target currency -> ').upper()
 result = rates.convert(source_currency, target_currency, amount)
