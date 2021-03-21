156514cb622a5b4d617ff324c38199c29577ab81
dmitrymorozov <morozovd@me.com>
2021-03-21 19:39:16 +0300

    Currency table added.

--- Lesson_3/cur_converter.py
+++ Lesson_3/cur_converter.py
@@ -1,6 +1,11 @@
 from forex_python.converter import CurrencyRates
 
 rates = CurrencyRates()
+if input('Do you need currency exchange table ? y/n -> ').lower() == 'y':
+    cur_table = rates.get_rates('RUB')
+    print ( cur_table)
+    for cur in cur_table.keys():
+        print(f'{cur}   :   {rates.get_rate(cur, "RUB"):.2f} Rub.')
 
 amount = float(input('Enter amount -> '))
 source_currency = input('Enter the source currency -> ').upper()
